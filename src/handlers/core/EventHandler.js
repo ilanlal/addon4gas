be3634ca38dd029c3fe1cf59085d28a29e382c1f
Ilan Laloum <isramail@gmail.com>
2025-12-17 23:41:17 +0200

    feat: Refactor EventHandler to use ViewModel for card navigation and update tests accordingly

--- src/handlers/core/EventHandler.js
+++ src/handlers/core/EventHandler.js
@@ -1,3 +1,5 @@
+const { onOpen } = require("../../Code");
+
 class EventHandler {
     get activeSpreadsheet() {
         if (!this._activeSpreadsheet) {
@@ -35,98 +37,30 @@ class EventHandler {
     }
 };
 
-EventHandler.Controller = {
+EventHandler.ViewModel = {
     onOpenHomeCard: (e) => {
-        return new EventHandler
-            .Wrapper(
-                EventHandler.prototype.activeSpreadsheet, EventHandler.prototype.documentProperties, EventHandler.prototype.userProperties, EventHandler.prototype.scriptProperties)
-            .handleOpenHomeCard(e);
+        return NavigationHandler.ViewModel
+            .onPushCardClick({ parameters: { template: 'EMD.Cards.Home' } });
     },
     onOpenAccountCard: (e) => {
-        return new EventHandler
-            .Wrapper(
-                EventHandler.prototype.activeSpreadsheet, EventHandler.prototype.documentProperties, EventHandler.prototype.userProperties, EventHandler.prototype.scriptProperties)
-            .handleOpenAccountCard(e);
+        return NavigationHandler.ViewModel
+            .onPushCardClick({ parameters: { template: 'EMD.Cards.Account' } });
     },
     onOpenAboutCard: (e) => {
-        return new EventHandler
-            .Wrapper(
-                EventHandler.prototype.activeSpreadsheet, EventHandler.prototype.documentProperties, EventHandler.prototype.userProperties, EventHandler.prototype.scriptProperties)
-            .handleOpenAboutCard(e);
+        return NavigationHandler.ViewModel
+            .onPushCardClick({ parameters: { template: 'EMD.Cards.About' } });
+    },
+    onOpenHelpCard: (e) => {
+        return NavigationHandler.ViewModel
+            .onPushCardClick({ parameters: { template: 'EMD.Cards.Help' } });
     },
     onActivatePremiumClicked: (e) => {
-        return new EventHandler
-            .Wrapper(
-                EventHandler.prototype.activeSpreadsheet, EventHandler.prototype.documentProperties, EventHandler.prototype.userProperties, EventHandler.prototype.scriptProperties)
-            .handleActivatePremiumClicked(e);
+        throw new Error("Not implemented yet");
     },
     onRevokeLicenseClicked: (e) => {
-        return new EventHandler
-            .Wrapper(
-                EventHandler.prototype.activeSpreadsheet, EventHandler.prototype.documentProperties, EventHandler.prototype.userProperties, EventHandler.prototype.scriptProperties)
-            .handleRevokeLicenseClicked(e);
+        throw new Error("Not implemented yet");
     }
 }
-EventHandler.Wrapper = class {
-    constructor(activeSpreadsheet, documentProperties, userProperties, scriptProperties) {
-        this._activeSpreadsheet = activeSpreadsheet;
-        this._documentProperties = documentProperties;
-        this._userProperties = userProperties;
-        this._scriptProperties = scriptProperties;
-    }
-
-    handleOpenHomeCard(e) {
-        return NavigationHandler.ViewModel
-                .onPushCardClick({ parameters: { template: 'EMD.Cards.Home' } });
-    }
-
-    handleOpenAccountCard(e) {
-        return NavigationHandler.ViewModel
-                .onPushCardClick({ parameters: { template: 'EMD.Cards.Account' } });
-    }
-
-    handleOpenAboutCard(e) {
-        return NavigationHandler.ViewModel
-                .onPushCardClick({ parameters: { template: 'EMD.Cards.About' } });
-    }
-
-    handleActivatePremiumClicked(e) {
-        try {
-            // return "Not implemented yet" error
-            throw new Error("Not implemented yet");
-        } catch (error) {
-            return this.handleError(error)
-                .build();
-        }
-    }
-
-    handleRevokeLicenseClicked(e) {
-        try {
-            // return "Not implemented yet" error
-            throw new Error("Not implemented yet");
-        } catch (error) {
-            return this.handleError(error)
-                .build();
-        }
-    }
-
-    handleOperationSuccess(message) {
-        // Show a success message to the user
-        return CardService.newActionResponseBuilder()
-            .setNotification(
-                CardService.newNotification()
-                    .setText(message));
-    }
-
-    handleError(error) {
-        // Show an error message to the user
-        return CardService.newActionResponseBuilder()
-            .setNotification(
-                CardService.newNotification()
-                    .setText(
-                        error.toString()));
-    }
-};
 
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = {
